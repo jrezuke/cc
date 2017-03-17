4c8c6c3968bb4247eea36fe587059be100dcf5b9
jrezuke <j.rezuke@verizon.net>
2017-03-17 15:57:37 -0400

    menu for fluid-infusins... need to be in entry.component

--- src/app/layout/layout-bar/layout-bar.component.ts
+++ src/app/layout/layout-bar/layout-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LayoutBarItem } from './layout-bar-item/layout-bar-item';
-import { Route } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'layout-bar',
@@ -11,14 +11,17 @@ export class LayoutBarComponent implements OnInit {
   @Input() layoutBarItems: LayoutBarItem[];
   @Input() expanded: boolean;
 
-  constructor() { }
+  constructor(private _router:Router, private _activatedRoute:ActivatedRoute) { }
 
   ngOnInit() {
     console.log("LayoutBarComponent.ngOnInit expanded", this.expanded);
   }
 
-  onItemClick(item: LayoutBarItem){
-    console.log("LayoutBarComponent.onItemClick", item);
-  }
+  // onItemClick(item: LayoutBarItem){
+  //   console.log("LayoutBarComponent.onItemClick", item);
+  //   console.log("actvRoute:", this._activatedRoute);
+  //   let path = "/" + item.route as any;
+  //   //this._router.navigate(['home'], {relativeTo:this._activatedRoute})
+  // }
 
 }
